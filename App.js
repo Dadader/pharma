568e81cdca5cc80f8585de7bfb3fd52908b5d159
Dadader <54673254+Dadader@users.noreply.github.com>
2023-06-04 01:55:32 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View,Text} from 'react-native';
+import HomeTabs from './src/HomeTabs';
+import Header from './src/Header';
+
+// const Stack = createNativeStackNavigator();
+
+function App(){
+
+  return (    
+      <View style={{flex:1}}>
+        <Header/>
+        <HomeTabs/>
+      </View>
+  );
+}
+
+export default App;
